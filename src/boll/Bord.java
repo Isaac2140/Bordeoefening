88e07a213ce426cb45befc8458566811afd897fc
Isaac2140 <78203152+Isaac2140@users.noreply.github.com>
2021-05-11 15:30:04 +0200

    added new line

--- src/boll/Bord.java
+++ src/boll/Bord.java
@@ -4,6 +4,7 @@ public class Bord implements Oppervlakte {
     private String materiaal;
     private String kleur;
     public Bord(String materiaal, String kleur){
+
         this.materiaal = materiaal;
         this.kleur = kleur;
     }
